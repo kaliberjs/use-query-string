9163786f10c2513a837fdbdac46e889ba710f235
Peeke Kuepers <pkuepers@kaliber.net>
2021-05-11 10:22:31 +0200

    Better name for the search query, to avoid confusion with location.search

--- example/src/App.js
+++ example/src/App.js
@@ -9,16 +9,16 @@ export default function AppWithProviders({ search }) {
 }
 
 function App() {
-  const [{ search }, setQueryString] = useQueryString({ search: '' })
+  const [{ search: searchQuery }, setQueryString] = useQueryString({ search: '' })
   const [input, setInput] = React.useState(null)
 
   return (
     <form onSubmit={handleSubmit}>
-      {search
-        ? <h1>You've searched on '{search}'</h1>
+      {searchQuery
+        ? <h1>You've searched on '{searchQuery}'</h1>
         : <h1>Search</h1>
       }
-      <input type='text' value={input ?? search} onChange={e => setInput(e.currentTarget.value)} name='search' />
+      <input type='search' value={input ?? searchQuery} onChange={e => setInput(e.currentTarget.value)} name='search' />
       <button type='submit'>Apply search query</button>
     </form>
   )
